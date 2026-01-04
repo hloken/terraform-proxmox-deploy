ac08a8ebc6b05d97537a246dcc6aaf1cf7a64a22
Hans Løken <hans@ebamse.net>
2026-01-04 13:09:00 +0100

    Added files for tf variables and cluster config

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "cluster_name" {
+  type    = string
+  default = "homelab"
+}
+
+variable "default_gateway" {
+  type    = string
+  default = "10.0.0.1"
+}
+
+variable "talos_cp_01_ip_addr" {
+  type    = string
+  default = "10.1.1.10"
+}
+
+variable "talos_worker_01_ip_addr" {
+  type    = string
+  default = "10.1.1.11"
+}
